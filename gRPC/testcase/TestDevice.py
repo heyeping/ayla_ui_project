bc97288146700d5c356b7af35f4acfb2e20ff095
Yeping He <heyeping@sunseaaiot.com>
2021-04-13 16:54:33 +0800

    新增grpc相关及自动化安装apk脚本

---
+++ gRPC/testcase/TestDevice.py
@@ -0,0 +1,9 @@
+#@project:  ayla_ui_project
+#@author: heyeping
+#@file: TestDevice.py.py
+#@ide: PyCharm
+#@time: 2021/4/2 5:30 PM
+
+import pytest
+from gRPC.client.Device_client import Device_client
+
---
+++ gRPC/testcase/__init__.py
@@ -0,0 +1,7 @@
+#@project:  ayla_ui_project
+#@author: heyeping
+#@file: __init__.py.py
+#@ide: PyCharm
+#@time: 2021/4/2 5:29 PM
+
+
